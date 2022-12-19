2c0e689db83256bdbd8bbbe5441dc9d229bffafa
rayala144 <rayalaakhil1999@gmail.com>
2022-12-19 11:22:51 +0530

    Empty commit message

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,7 @@
 import { Component, NgModule } from '@angular/core';
 import { Router, RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-  {
-    path: '',
-    
-    }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
